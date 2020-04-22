dd88385bf0c56206ca077eed8036d364b8ca41d0
PyeongGang-Kim <pyeonggangkim@gmail.com>
2020-04-22 23:50:03 +0900

    .
    Former-commit-id: 318486dcac4d4263e81a9ccfc66eaa7fd5b51ee5

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,57 +1,57 @@
 // import axios from "axios";
-import http from "./http"
+import https from "./http"
 
 const apiUrl = "/api";
 const regiUrl = "/rest-auth";
 
 export default {
   getStores(params) {
-    return http.get(`${apiUrl}/stores`, {
+    return https.get(`${apiUrl}/stores`, {
       params
     });
   },
   getUserReview(params) {
     // console.log("api index ", params.user_id)
-    return http.get(`${apiUrl}/user_reviews`, {
+    return https.get(`${apiUrl}/user_reviews`, {
       params
     });
   },
   register(params) {
     // console.log(params)
-    return http.post(`${apiUrl}${regiUrl}/registration/`, params)
+    return https.post(`${apiUrl}${regiUrl}/registration/`, params)
   },
   login(params) {
-    return http.post(`${apiUrl}/login/`, params)
+    return https.post(`${apiUrl}/login/`, params)
   },
   reviewDelete(params) {
-    return http.delete(`${apiUrl}/user_reviews/${params}`)
+    return https.delete(`${apiUrl}/user_reviews/${params}`)
   },
   reviewUpdate(params) {
     // console.log(params)
-    return http.put(`${apiUrl}/user_reviews/${params["id"]}`, params)
+    return https.put(`${apiUrl}/user_reviews/${params["id"]}`, params)
   },
   getStoresByLocation(params) {
-    return http.post(`${apiUrl}/search_store`, params)
+    return https.post(`${apiUrl}/search_store`, params)
   },
   editReview(params) {
-    return http.put(`${apiUrl}/store_reviews/${params.id}`, params)
+    return https.put(`${apiUrl}/store_reviews/${params.id}`, params)
   },
   getStoreReview(params) {
-    return http.get(`${apiUrl}/get_store_reviews_by_store_id/${params}`, params)
+    return https.get(`${apiUrl}/get_store_reviews_by_store_id/${params}`, params)
   },
   writeReview(params) {
-    return http.post(`https://i02d106.p.ssafy.io:8765/api/store_reviews`, params)
+    return https.post(`https://i02d106.p.ssafy.io:8765/api/store_reviews`, params)
   },
   setUserCategory(params) {
     const headers = {
       Authorization: "jwt " + localStorage.getItem("token")
     }
-    return http.post(`${apiUrl}/set_user_category`, params, {headers})
+    return https.post(`${apiUrl}/set_user_category`, params, {headers})
   },
   getUserBasedRecommand() {
     const headers = {
       Authorization: "jwt " + localStorage.getItem("token")
     }
-    return http.get(`${apiUrl}/user_based_cf`, {headers})
+    return https.get(`${apiUrl}/user_based_cf`, {headers})
   }
 };
