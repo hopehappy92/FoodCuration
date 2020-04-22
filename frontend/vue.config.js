bb75ce6a191bf0047947a875e3abe1a8f390f75d
김평강 <pyeonggangkim@gmail.com>
2020-04-22 19:26:50 +0900

    ,
    Former-commit-id: 35597ccbb01c7468d70776254b8b67a3f8e29345

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -7,11 +7,5 @@ module.exports = {
       }
     }
   },
-  transpileDependencies: ["vuetify"],
-  configureWebpack: {
-    devServer: {
-        host: 'http://i02d106.p.ssafy.io',
-        port: '80'
-    }
-}
+  transpileDependencies: ["vuetify"]
 };
