3de4732ceaa0696b5f8aefa7b4f09d9592d9a5c7
PyeongGang-Kim <pyeonggangkim@gmail.com>
2020-04-22 23:44:27 +0900

    .
    Former-commit-id: 71fe4f6323ba10e79cb3e4f4301a3fb40ca6f05b

--- backend/api/views.py
+++ backend/api/views.py
@@ -174,7 +174,7 @@ def trend_by_tob(self):
     return Response(tob_dict)
 
 def go_to_myhome(request):
-    return redirect("http://i02d106.p.ssafy.io/")
+    return redirect("https://i02d106.p.ssafy.io/")
 
 class CustomLoginView(LoginView):
     def get_response(self):
