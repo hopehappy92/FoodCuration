90c8034bfbaf3c181e55240ddf5ba38cb40d73ab
김사덕 <tkejr474@naver.com>
2020-04-22 17:34:13 +0900

    컨텐츠 기반 필터링 구현
    - S02P23D106-78
    - 컨텐츠 기반 필터링 완료
    
    
    Former-commit-id: 5fb1ccef59561273e551c3cc54876d0c7b2fa16a

--- backend/knn_item_based.py
+++ backend/knn_item_based.py
@@ -23,95 +23,69 @@ from api.models import Store #상점 46만개
 from api.models import Review # 리뷰 9만개
 from api.models import CustomUser # 유저 1.8만개
 
-# # review가 10개 이상인 식당만 불러옴
-# request_store = requests.get("http://i02d106.p.ssafy.io:8765/api/store/10").json()
-# # review가 10개 이상인 유저만 불러옴
-# request_user = requests.get("http://i02d106.p.ssafy.io:8765/api/user").json()
-# 모든 review 다 불러옴
 
-all_store = Store.objects.all().values("id", "latitude", "longitude", "category")
-all_review = Review.objects.all().values("store_id", "score")
+def recoNearStroe(temp_id):
+    # 초기 데이터 받아와
+    all_store =pd.DataFrame(Store.objects.all().values("id", "latitude", "longitude", "category"))
+    min_review = 5
 
-new_df = pd.DataFrame(columns=['category', 'store_id'])
+    # 원하는 id에 해당하는 삭당 정보 받아와
+    near_store = pd.DataFrame(columns=["id", 'category', 'category_count', 'calc'])
+    temp_store = all_store.loc[all_store['id'] == temp_id]
+    temp_store_name = temp_store['id']
+    clat = temp_store['latitude'].values[0]
+    clon = temp_store['longitude'].values[0]
 
-near_store = set()
-category_set = set()
-clat =37.5
-clon = 126.9
-count = 0
-all_store
-
-temp_store_name = 213012
-temp_category_set = set()
-for store in all_store:
-    if(store['id']==temp_store_name):
-        categories = store["category"].split("|")
-        for category in categories:
-            temp_category_set.add(category)
-
-print(len(temp_category_set))
-for store in all_store:
-    lat = store['latitude']
-    lon = store['longitude']
-    if 6371*acos(cos(radians(lat))*cos(radians(clat))*cos(radians(clon)-radians(lon))+sin(radians(lat))*sin(radians(clat))) < 1:
-        near_store.add(store['id'])
-        categories = store["category"].split("|")
-        for category in categories:
-            category_set.add(category)
-            new_df.loc[len(new_df)] = [category, store['id']]
-
-# store에서 category값만 분류
-print(len(near_store))
-print(near_store)
-print(len(category_set))
-print(len(new_df))
-print(category_set)
-near_store_review = list()
-
-for review in all_review:
-    if review['store_id'] in near_store:
-        near_store_review.append(review)
-
-# 근처에 있는 식당들의 review만 가져옴
-print(len(near_store_review))
+    # 해당 식당의 카테고리 먼저 가져와
+    category_set = set(temp_store['category'].values[0].split('|'))
+    
+    # 이게 잘 안 먹히네요
+    # all_store = all_store.loc[6371*acos(cos(radians(all_store['latitude']))*cos(radians(clat))*cos(radians(clon)-radians(all_store['longitude']))+sin(radians(all_store['latitude']))*sin(radians(clat))) < 1]
 
 
+    # for문이 속도가 느려 전처리를 좀 했습니다. 정사각형 약 1km반경으로 가져왔습니다
+    con1 = (all_store['longitude']<(0.01+clon))
+    con2 = ((clon -0.01) <all_store['longitude'])
+    con3 = (all_store['latitude']<(0.01+clat))
+    con4 = ((clat -0.01) <all_store['latitude'])
+    all_store = all_store[con1 & con2 & con3 & con4]
 
+    # 반경 1km 내에 있으면 category 겹치는게 몇개인지 계산
+    for i in range(len(all_store)):
+        store = all_store.iloc[i]
+        lat = store['latitude']
+        lon = store['longitude']
+        if 6371*acos(cos(radians(lat))*cos(radians(clat))*cos(radians(clon)-radians(lon))+sin(radians(lat))*sin(radians(clat))) < 1:
+            if(store['id']==temp_id):
+                continue
+            categories = store["category"].split('|')
+            count = 0
+            for c in categories:
+                if c in category_set:
+                    count += 1
+            near_store.loc[len(near_store)] = [store['id'], store['category'], count, 0]
 
-request_all_review = pd.DataFrame(near_store_review)
-reader = Reader(rating_scale=(1, 5))
-request_all_review = surprise.Dataset.load_from_df(df=request_all_review, reader=reader)
-trainset = request_all_review.build_full_trainset()
-sim_options = {'name': 'pearson_baseline', 'user_based': False}
-algo = KNNBaseline(sim_options=sim_options)
-algo.fit(trainset)
+    # 필요한 review들만 가져옴
+    review_df = pd.DataFrame(Review.objects.all().values("score", "store_id"))
+    review_df = review_df[review_df['store_id'].isin(set(near_store['id']))]
 
-print('완료')
+    review_df = review_df.groupby('store_id').agg(['sum', 'count', 'mean'])['score']
+    a = sum(review_df['sum'])/sum(review_df['count'])
 
+    # 인기도 고려한 평점 계산
+    review_df['calc'] = review_df.apply(lambda x: ((x['count']/(x['count']+min_review))*x['mean'] + (min_review/(x['count']+min_review))*a), axis=1)
 
+    for i in review_df.index:
+        val = review_df.loc[i]
+        store_calc = val['calc']
+        near_store.loc[near_store['id']==i, 'calc'] = store_calc
 
-# Read the mappings raw id <-> movie name
-rid_to_name, name_to_rid = read_item_names()
+    # 카테고리 일치 개수, 인기도 고려한 평점 순 정렬
+    near_store.sort_values(by=['category_count', 'calc'], inplace=True, ascending=False)
 
-# Retrieve inner id of the movie Toy Story
-toy_story_raw_id = name_to_rid['Toy Story (1995)']
-toy_story_inner_id = algo.trainset.to_inner_iid(toy_story_raw_id)
+    return near_store
 
-# Retrieve inner ids of the nearest neighbors of Toy Story.
-toy_story_neighbors = algo.get_neighbors(toy_story_inner_id, k=10)
-
-# Convert inner ids of the neighbors into names.
-toy_story_neighbors = (algo.trainset.to_raw_iid(inner_id)
-                       for inner_id in toy_story_neighbors)
-toy_story_neighbors = (rid_to_name[rid]
-                       for rid in toy_story_neighbors)
-
-print()
-print('The 10 nearest neighbors of Toy Story are:')
-for movie in toy_story_neighbors:
-    print(movie)
-
-
+print(recoNearStroe(124164))
 
 
 
