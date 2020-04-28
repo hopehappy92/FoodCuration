2cef366e48454641ee983c411c9347efe8c67328
김평강 <pyeonggangkim@gmail.com>
2020-04-28 11:53:50 +0900

    배포서버 최신화
    Former-commit-id: 5a574445c841fb4ff34470a70ed4f94940a4294d [formerly 7fd64db3414d0a584efcec10fa84c47ce58a53fc]
    Former-commit-id: 0bac7f887213b1c2480c54ba6ab194238a2c0d9a

--- backend/api/views.py
+++ backend/api/views.py
@@ -305,7 +305,6 @@ class StoreReviewSet(viewsets.ModelViewSet):
 
     def create(self, request):
         # 리뷰를 작성하는 함수입니다.
-        print('asdf')
         if request.user.is_authenticated:
             data = request.data
             store = Store.objects.get(id=data["store"])
@@ -316,21 +315,7 @@ class StoreReviewSet(viewsets.ModelViewSet):
             user.review_count += 1
             user.save()
             return Response("작성 성공")
-        else:
-            return Response("작성 실패")
-        # data = request.data
-        # store = Store.objects.get(id=data["store"])
-        # store_name = store.store_name
-        # user = CustomUser.objects.get(id=data["user"])
-        # # 받아온 데이터를 이용해서 Review를 작성합니다.
-        # models.Review.objects.create(store_id=data["store"], user_id=data["user"], content=data["content"], score=data["score"], reg_time=datetime.datetime.now(), store_name=store_name)
-        
-        # 작성이 완료되었다면 매장과 유저의 review_count를 1씩 추가합니다.
-        # store.review_count += 1
-        # store.save()
-        # user.review_count += 1
-        # user.save()
-        return Response("작성 성공")
+        return Response("작성 실패")
 
     def update(self, request, pk=None):
         '''
